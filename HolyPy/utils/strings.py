a5e8a1f193eeda1f714a8b7061ce9608280b0273
RageYL <vic1708@gmail.com>
2014-02-23 17:51:29 +0100

    strings

--- HolyPy/utils/strings.py
+++ HolyPy/utils/strings.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python2
 # coding: utf-8
 
+from holypy.utils.maths import ceil_modulus
+
 ################################################################################
 ### Methods
 ################################################################################
 
+def pad(string, char, chunk, ljust = True):
+    if ljust:
+        return string.ljust(ceil_modulus(len(string), chunk), char)
+    else:
+        return string.rjust(ceil_modulus(len(string), chunk), char)
 
 ################################################################################
 ### Module
